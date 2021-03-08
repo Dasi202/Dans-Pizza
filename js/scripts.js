a80964cd719674c0dbced30eabde39f6f30658d5
Danlon Situma <situmadanlon@gmail.com>
2021-03-08 05:39:47 +0300

    Add jquery functionality

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+
+var size, crust, toppings, number;
+$(document).ready(function () {
+    $("#order").click(function (event) {
+        event.preventDefault();
+        size = parseInt(document.getElementById('sizes').value);
+        crust = parseInt(document.getElementById('crusts').value);
+        toppings = parseInt(document.getElementById('toppings').value);
+        number = parseInt(document.getElementById('number').value);
+
+        var total = size + crust + toppings;
+        var grandTotal = total * number;
+        console.log(toppings);
+        console.log(grandTotal);
+
+        $("ul#list").append(
+
+            "<li>" + "Your pizza size is : " + $("#sizes option:selected").text() + "</li>" +
+            "<li>" + "Your pizza crust is : " + $("#crusts option:selected").text() + "</li>" +
+            "<li>" + "Your pizza topping is : " + $("#toppings option:selected").text() + "</li>" +
+            "<li>" + "Number of pizza ordered is : " + number + "</li>" +
+            "<li>" + "Total amount to be paid is : Ksh" + grandTotal + "</li>");
+
+    })
+
+    $(".deliver").click(function () {
+        var fields = prompt("Enter your address")
+        prompt("Enter your name")
+        prompt("Enter your location")
+        alert("Your order will be delivered at" + fields)
+        alert("Delivery cost is Kshs 100")
+    })
+    $(".delivernot").click(function () {
+        swal({
+            text: "Pick your order in our nearest shop",
+            icon: "success"
+        })
+    })
+
+    $('.parent-container').magnificPopup({
+        delegate:'a',//child items selector, by clicking on it popup will open
+        type: 'image',
+        closeOnContentClick: true,
+        //other options
+      });
+});
